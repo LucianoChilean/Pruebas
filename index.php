a6923a124ffc9d28ff4da12a9adb0a55180d516c
Paula Aros <paula.arosh@gmail.com>
2019-08-01 17:03:23 -0400

    holi

--- index.php
+++ index.php
@@ -9,3 +9,17 @@ $manzana = 'que sapa';
 echo 'hola cauros',$manzana;
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        hola :D <?php echo $manzana ?>
+    </div>
+</body>
+</html>
